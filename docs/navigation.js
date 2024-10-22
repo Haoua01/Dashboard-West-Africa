e2cad0cf686eca8cfa144fb0d67a8a688d75edc5
Haoua01 <haoua.benaliabbo@telecom-paris.fr>
2024-10-22 21:57:29 +0200

    add the second indicator

--- docs/navigation.js
+++ docs/navigation.js
@@ -16,32 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
     // Show map1 and map3 by default
     document.getElementById('map1').style.display = 'block'; // Show map1
     document.getElementById('map3').style.display = 'block'; // Show map3
-
-    // Add event listeners for the first set of toggle buttons (maps 1 and 2)
-    const toggleButtons1 = document.querySelectorAll('.toggle-buttons:first-of-type button');
-    toggleButtons1.forEach((button) => {
-        button.addEventListener('click', function() {
-            toggleMap(this.textContent.includes('Normalisation par pays') ? 'map1' : 'map2');
-        });
-    });
-
-    // Add event listeners for the second set of toggle buttons (maps 3 and 4)
-    const toggleButtons2 = document.querySelectorAll('.toggle-buttons:last-of-type button');
-    toggleButtons2.forEach((button) => {
-        button.addEventListener('click', function() {
-            toggleMap(this.textContent.includes('Résultats globaux') ? 'map3' : 'map4');
-        });
-    });
 });
 
 // Function to toggle visibility of the maps
-function toggleMap(mapId) {
-    // Hide all maps
-    var maps = document.getElementsByClassName('map');
-    for (var i = 0; i < maps.length; i++) {
-        maps[i].style.display = 'none';
-    }
+function toggleMap(showMapId, hideMapId) {
+    // Hide the specified map
+    document.getElementById(hideMapId).style.display = 'none';
     
-    // Show the selected map
-    document.getElementById(mapId).style.display = 'block';
+    // Show the specified map
+    document.getElementById(showMapId).style.display = 'block';
 }
