5a9b991bc5cd8bfb658a11ddd8a60b7a6fff1892
Haoua01 <haoua.benaliabbo@telecom-paris.fr>
2024-10-26 20:31:45 +0200

    changement de representation map group 2

--- docs/navigation.js
+++ docs/navigation.js
@@ -1,24 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Initialiser la carte par défaut sur 'map1' pour le Bénin et afficher 'chart3'
-    document.getElementById('map1').style.display = 'block'; // Afficher la première carte
-    document.getElementById('chart3').style.display = 'block'; // Afficher le diagramme par défaut
+    // Afficher uniquement 'map1' de group1 au démarrage
+    document.getElementById('map1').style.display = 'block'; // Afficher la première carte par défaut
+    document.getElementById('countryDropdown').style.display = 'block'; // Afficher le menu déroulant associé
 
-    // Activer le bouton par défaut
+    // Masquer tous les éléments de group2 au démarrage
+    const group2Elements = document.querySelectorAll('.group2');
+    group2Elements.forEach(element => {
+        element.style.display = 'none';
+    });
+
+    // Activer le bouton par défaut pour 'map1'
     setActiveButton('map1');
 
     // Charger la carte du Bénin dans map1 par défaut
     const iframe = document.getElementById('map-frame');
     iframe.src = 'isibf_benin.html'; // Carte par défaut pour le Bénin
-
-    // Afficher le menu déroulant pour 'Normalisation par pays'
-    document.getElementById('countryDropdown').style.display = 'block';
 });
 
 // Fonction pour basculer l'affichage des cartes et des histogrammes
 function toggleMap(showMapId) {
+    // Masquer tous les éléments de group1 et group2
     const mapsAndCharts = document.querySelectorAll('.map, .chart');
     mapsAndCharts.forEach(element => {
-        element.style.display = 'none'; // Masquer toutes les cartes et les histogrammes
+        element.style.display = 'none';
     });
     
     // Afficher la carte ou l'histogramme sélectionné
