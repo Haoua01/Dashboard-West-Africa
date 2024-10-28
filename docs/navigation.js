473343e45fb5f7456b08882220e9d45bb7eef707
Haoua01 <haoua.benaliabbo@telecom-paris.fr>
2024-10-28 11:58:37 +0100

    rajout des cartes pour la normalisation globale

--- docs/navigation.js
+++ docs/navigation.js
@@ -51,6 +51,9 @@ function toggleGroup1(showMapId) {
     } else if (showMapId === 'map2') {
         document.getElementById('countryDropdown11').style.display = 'none'; // Masquer dropdown pour map1
         document.getElementById('countryDropdown12').style.display = 'block'; // Afficher dropdown pour map2
+        // Charger la carte par défaut dans map2
+        const iframe = document.getElementById('map2-frame');
+        iframe.src = 'benin_map.html'; // ou une autre carte par défaut
     }
 }
 
