3355e5d6144e6dac444c0e80fa2a65baa4c37042
Haoua01 <haoua.benaliabbo@telecom-paris.fr>
2024-11-03 22:08:00 +0100

    change directory to results

--- object-oriented/map_visualizer.py
+++ object-oriented/map_visualizer.py
@@ -82,7 +82,7 @@ class MapVisualizer:
         )
 
         # Save as HTML
-        html_file_path = f'/Users/haouabenaliabbo/Desktop/M2 IREN/ALTERNANCE/GitHub/Dashboard-West-Africa/docs/results/{self.label}_{self.country}.html'
+        html_file_path = f'/Users/haouabenaliabbo/Desktop/M2 IREN/ALTERNANCE/GitHub/Dashboard-West-Africa/map/docs/results/{self.label}_{self.country}.html'
 
         # Save the map as HTML
         fig.write_html(html_file_path)
