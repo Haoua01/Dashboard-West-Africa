b5deaeac3da5cd2a62ca164e3d7ad017e835526b
Haoua01 <haoua.benaliabbo@telecom-paris.fr>
2024-11-11 00:03:26 +0100

    add department map

--- docs/navigation.js
+++ docs/navigation.js
@@ -142,3 +142,16 @@ function updateSliderValue() {
         departmentLabel.style.fontWeight = 'normal'; // Remove highlight from "Départements"
     }
 }
+
+let background_box = document.querySelector(".background_box");
+    let toggle_box = document.querySelector(".toggle_box");
+    let circle = document.querySelector(".circle");
+    let checkbox = document.getElementById("checkbox");
+
+    toggle_box.onclick = function(){
+        if(checkbox.checked){
+            circle.style.transform = "translateX(100px)";
+        }else{
+            circle.style.transform = "translateX(0px)";
+        }
+    }
